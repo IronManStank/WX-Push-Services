8f077caafb0ec68587571fb7c46105c43ada8fcd
L.S <1157723200@qq.com>
2023-02-25 10:25:32 +0800

    服务上线

---
+++ Readme.md
@@ -0,0 +1,71 @@
+# 怎样使用微信以及微信推送服务
+
+[TOC]
+
+
+
+## 实现微信推送服务的两种方式
+
+1. 调用企业微信应用
+
+使用该种方法时请按照下文方式获得必要token。
+
+2. 使用`web hook`调用企业微信机器人
+
+使用该种方式时仅需要知道企业微信机器人的`web hook key`。在使用之前，请确保已在企业微信群里中添加企业微信机器人，并按照下文方法获得`web hook key`。
+
+# 使用方式
+
+### 1. 注册企业微信，获取必要token
+
+- 使用企业微信应用时获取以下信息：
+
+注册网址：https://work.weixin.qq.com/
+
+1. 获取企业ID
+
+![image-20230225000159967](https://s2.loli.net/2023/02/25/9V3l5IGvZiFqMRu.png)
+
+在仓库`secrets`中添加如下变量：`CORP_ID=your_id`
+
+2. 添加应用并获取下述变量
+
+![image-20230225100535470](https://s2.loli.net/2023/02/25/XaTm65MjOE3A8iJ.png)
+
+![image-20230225100751916](https://s2.loli.net/2023/02/25/bkJGwyzZfgIOa7R.png)
+
+在仓库`secrets`中添加：`APP_SECRET=your_app_secret`以及`APP_ID=your_app_id`
+
+- 使用机器人`webhook`方式时，请添加以下变量：
+
+![image-20230225101235709](https://s2.loli.net/2023/02/25/gOtL3dmJqpBDWIh.png)
+
+![image-20230225101249931](https://s2.loli.net/2023/02/25/bghHpI3UDvq29lM.png)
+
+找到群聊，在其中点击机器人配置，获取`webhookkey`中`key`字段；在仓库中添加`key=your_web_hook_key`
+
+至此，必要信息已手机完毕。
+
+### 2. 安装python运行库
+
+执行`pip install -r requirments.txt`
+
+### 3. 运行示例`demo`
+
+```python
+    # wxps = APP_PUSH()
+    hookps = WEB_HOOK_PUSH()
+    
+    test = (
+        "# 企业微信消息测试"
+        + "## •  二级标题"
+        + "## •  测试通过"
+    )
+    # wxps.send_message(message=test, markdown=False)
+    hookps.send_message(message=test, markdown=False)
+```
+
+## 注意事项
+
+**经过测试。markdown信息并不能在微信查看。只能在企业微信查看。所以，如果想要在微信查看，需要将markdown类型消息改为text类型**
+
---
+++ main.py
@@ -0,0 +1,191 @@
+import os
+import json
+import requests
+import configparser
+from retry import retry
+
+
+class APP_PUSH(object):
+    def __init__(self) -> None:
+        """_summary_
+        generate a instance of APP_PUSH,init config
+        """
+        self.touser = "@all"
+        try:
+            # 环境变量读取
+            self.__corp_id = os.environ["CORP_ID"]
+            self.__app_secret = os.environ["APP_SECRET"]
+            self.__app_id = os.environ["APP_ID"]
+
+        except KeyError:
+            # 本地读取
+            print("未检测到环境变量，尝试使用本地配置文件中")
+            config = self.get_config()
+            print(config)
+            self.__app_id, self.__app_secret, self.__corp_id = config
+
+    @retry(tries=3, delay=1)
+    def _get_token(self) -> str:
+        """_summary_
+
+        Raises:
+            Exception: get token error and retry
+
+        Returns:
+            str: token
+        """
+        url = r"https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+        params = {
+            "corpid": self.__corp_id,
+            "corpsecret": self.__app_secret,
+        }
+        response = requests.post(url, params)
+        data = json.loads(response.text)
+
+        if data["errmsg"] != "ok":
+            print(data)
+            raise Exception("获取token失败")
+        else:
+            data = json.loads(response.text)
+            return data["access_token"]
+
+    @retry(tries=3, delay=1)
+    def send_message(self, message, markdown=False) -> None:
+        """_summary_
+
+        Args:
+            message (_type_): string
+            markdown (bool, optional): _description_. Defaults to False.
+
+        Raises:
+            Exception: Senderror and retry
+        """
+        url = (
+            "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="
+            + self._get_token()
+        )
+        # 判断发送类型
+        if markdown:
+            send_values = {
+                "touser": self.touser,
+                "msgtype": "markdown",
+                "agentid": self.__app_id,
+                "markdown": {"content": message},
+                "safe": "0",
+            }
+        else:
+            send_values = {
+                "touser": self.touser,
+                "msgtype": "text",
+                "agentid": self.__app_id,
+                "text": {"content": message},
+                "safe": "0",
+            }
+
+        send_message = json.dumps(send_values).encode("utf-8")
+        response = requests.post(url, send_message)
+        status = response.json()["errmsg"]
+        if status != "ok":
+            # print(response.json())
+            raise Exception("消息发送失败")
+        else:
+            print("消息发送成功")
+
+    @staticmethod
+    def get_config() -> tuple:
+        """_summary_
+            获取本地配置文件中的配置信息
+        Returns:
+            _type_: tuple
+
+        """
+        try:
+            config = configparser.ConfigParser()
+            config.read("config.ini")
+            corp_id = config["Config"]["corp_id"]
+            app_id = config["Config"]["app_id"]
+            app_secret = config["Config"]["app_secret"]
+            return app_id, app_secret, corp_id
+        except Exception as e:
+            print("未检测到本地配置文件! 请配置必要文件！")
+
+
+class WEB_HOOK_PUSH:
+    def __init__(self) -> None:
+        """_summary_
+        init config
+        """        
+        self.header = {"Content-Type": "application/json;charset=UTF-8"}
+        self.url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send'
+        try:
+            self.key = os.environ["WEB_HOOK_BOT"]
+        except KeyError:
+            print("未检测到环境变量，尝试使用本地配置文件中")
+            self.key = self.get_config()
+
+
+    @retry(tries=3, delay=1)
+    def send_message(self, message, markdown=False) -> None:
+        """_summary_
+
+        Args:
+            message (str): message body
+            markdown (bool, optional): _description_. Defaults to False.
+
+        Raises:
+            Exception: Faild and retry
+        """        
+        params = {'key':self.key}
+        if markdown:
+            message_body = {
+                "msgtype": "markdown",
+                "markdown": {"content": message},
+                "at": {"atMobiles": [], "isAtAll": False},
+            }
+        else:
+            message_body = {
+                "msgtype": "text",
+                "text": {"content": message},
+                "at": {"atMobiles": [], "isAtAll": False},
+            }
+        send_data = json.dumps(message_body).encode("utf-8")
+        send_data = requests.post(self.url,params=params,data=send_data,headers = self.header)
+        status = send_data.json()["errmsg"]
+        if status  != "ok":
+            print(send_data)
+            raise Exception("消息发送失败")
+        else:
+            print("消息发送成功")
+        
+
+    @staticmethod
+    def get_config() -> str:
+        """_summary_
+
+        Returns:
+            str: hook key
+        """
+        try:
+            config = configparser.ConfigParser()
+            config.read("./config.ini")
+            web_hook_bot = config["Config"]["key"]
+            print(web_hook_bot)
+            return web_hook_bot
+        except Exception as e:
+            print("未检测到本地配置文件! 请配置必要文件！")
+
+def demo():
+    # wxps = APP_PUSH()
+    hookps = WEB_HOOK_PUSH()
+    
+    test = (
+        "# %s\n" % "消息推送展示项目：企业微信"
+        + "## •  环境：测试环境 \n"
+        + "## •  类型：%s \n" % "消息推送"
+        + "## •  测试结果：%s \n" % "通过"
+    )
+    # wxps.send_message(message=test, markdown=False)
+    hookps.send_message(message=test, markdown=False)
+
+if __name__ == "__main__":
+    demo()
