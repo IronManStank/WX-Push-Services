4dbcc3c2ef18c8f40a48f6490ddd08c61fa20aea
L.S <1157723200@qq.com>
2023-03-02 23:46:02 +0800

    自动检测配置文件

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ class APP_PUSH(object):
         """
         self.touser = "@all"
         try:
-            # 环境变量读取
+            # Read environment variables
             self.__corp_id = os.environ["CORP_ID"]
             self.__app_secret = os.environ["APP_SECRET"]
             self.__app_id = os.environ["APP_ID"]
 
         except KeyError:
-            # 本地读取
-            print("未检测到环境变量，尝试使用本地配置文件中")
+            # Read local configuration file
+            print(
+                "Configuration file is not detected, generating configuration file......"
+            )
             config = self.get_config()
-            print(config)
+            
             self.__app_id, self.__app_secret, self.__corp_id = config
 
     @retry(tries=3, delay=1)
@@ -39,14 +41,15 @@ class APP_PUSH(object):
             "corpid": self.__corp_id,
             "corpsecret": self.__app_secret,
         }
-        response = requests.post(url, params)
+        response = requests.post(url, json=params)
         data = json.loads(response.text)
 
         if data["errmsg"] != "ok":
-            print(data)
-            raise Exception("获取token失败")
+
+            raise Exception("Get token Failed!")
         else:
             data = json.loads(response.text)
+
             return data["access_token"]
 
     @retry(tries=3, delay=1)
@@ -64,7 +67,8 @@ class APP_PUSH(object):
             "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="
             + self._get_token()
         )
-        # 判断发送类型
+        # Determine  the send type
+
         if markdown:
             send_values = {
                 "touser": self.touser,
@@ -82,19 +86,19 @@ class APP_PUSH(object):
                 "safe": "0",
             }
 
-        send_message = json.dumps(send_values).encode("utf-8")
+        send_message = json.dumps(send_values)
         response = requests.post(url, send_message)
         status = response.json()["errmsg"]
         if status != "ok":
             # print(response.json())
-            raise Exception("消息发送失败")
+            raise Exception("Message Sent Failed!")
         else:
-            print("消息发送成功")
+            print("Message Sent Successfully!")
 
     @staticmethod
     def get_config() -> tuple:
         """_summary_
-            获取本地配置文件中的配置信息
+            Read loacl config file
         Returns:
             _type_: tuple
 
@@ -106,8 +110,8 @@ class APP_PUSH(object):
             app_id = config["Config"]["app_id"]
             app_secret = config["Config"]["app_secret"]
             return app_id, app_secret, corp_id
-        except Exception as e:
-            print(f"未检测到本地配置文件! 请配置必要文件！Err: {e}")
+        except Exception:
+            generate_config()
 
 
 class WEB_HOOK_PUSH:
@@ -116,11 +120,13 @@ class WEB_HOOK_PUSH:
         init config
         """
         self.header = {"Content-Type": "application/json;charset=UTF-8"}
-        self.url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send'
+        self.url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send"
         try:
             self.key = os.environ["WEB_HOOK_BOT"]
         except KeyError:
-            print("未检测到环境变量，尝试使用本地配置文件中")
+            print(
+                "No environment variables detected. Trying to use the local configuration file......"
+            )
             self.key = self.get_config()
 
     @retry(tries=3, delay=1)
@@ -134,7 +140,7 @@ class WEB_HOOK_PUSH:
         Raises:
             Exception: Faild and retry
         """
-        params = {'key': self.key}
+        params = {"key": self.key}
         if markdown:
             message_body = {
                 "msgtype": "markdown",
@@ -147,15 +153,16 @@ class WEB_HOOK_PUSH:
                 "text": {"content": message},
                 "at": {"atMobiles": [], "isAtAll": False},
             }
-        send_data = json.dumps(message_body).encode("utf-8")
+        send_data = json.dumps(message_body)
         send_data = requests.post(
-            self.url, params=params, data=send_data, headers=self.header)
+            self.url, params=params, data=send_data, headers=self.header
+        )
         status = send_data.json()["errmsg"]
         if status != "ok":
             print(send_data)
-            raise Exception("消息发送失败")
+            raise Exception("Message Sent Failed!")
         else:
-            print("消息发送成功")
+            print("Message Sent Successfully!")
 
     @staticmethod
     def get_config() -> str:
@@ -167,26 +174,54 @@ class WEB_HOOK_PUSH:
         try:
             config = configparser.ConfigParser()
             config.read("./config.ini")
-            web_hook_bot = config["Config"]["key"]
-            print(web_hook_bot)
-            return web_hook_bot
+            web_hook_bot_key = config["Config"]["key"]
+
+            return web_hook_bot_key
         except Exception as e:
-            print("未检测到本地配置文件! 请配置必要文件！")
+            generate_config()
+
+
+def generate_config() -> None:
+    """_summary_
+    Generate config file......
+    """
+    try:
+        size = os.path.getsize("./configtest.ini")
+        if size == 0:
+            raise FileNotFoundError
+    except FileNotFoundError:
+        print("Configuration file is not detected, generating configuration file......")
+        config = configparser.ConfigParser()
+        config["Config"] = {
+            "corp_id": "your corp_id # Enter your enterprise ID of wechat background here.",
+            "app_id": "your app_id # Enter your application ID of enterprise wechat background here",
+            "app_secret": "your app_secret # Enter your application secret of enterprise wechat background here",
+            "key": "your key # Enter the webhook key of the enterprise's wechat group chat robot here",
+        }
+
+        with open("./configtest.ini", "w") as f:
+            config.write(f)
+            print("Configuration file generated successfully!")
+    else:
+        print(
+            "The configuration file already exists. Please check whether the configuration file is correct."
+        )
 
 
 def demo():
-    # wxps = APP_PUSH()
-    hookps = WEB_HOOK_PUSH()
+    wxps = APP_PUSH()
+    # hookps = WEB_HOOK_PUSH()
 
     test = (
-        "# %s\n" % "消息推送展示项目：企业微信"
-        + "## •  环境：测试环境 \n"
-        + "## •  类型：%s \n" % "消息推送"
-        + "## •  测试结果：%s \n" % "通过"
+        "# %s\n" % "Message display test"
+        + "## •  TestMode: Markdown \n"
+        + "## •  Type: %s \n" % "Message Push"
+        + "## •  TestResult:%s \n" % "Pass"
     )
-    # wxps.send_message(message=test, markdown=False)
-    hookps.send_message(message=test, markdown=False)
+    wxps.send_message(message=test, markdown=False)
+    # hookps.send_message(message=test, markdown=False)
 
 
 if __name__ == "__main__":
+
     demo()
